463e90a8da6934f0d93b816710d1b1d368767540
Samvel <sam-lion@yandex.ru>
2025-06-22 00:14:58 +0500

    Внес правки в config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://samlion:mysecretpassword@localhost:5432/social_db"
+SQLALCHEMY_DATABASE_URI = "postgresql://samlion:Sam369333693@localhost:5432/social_network"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
